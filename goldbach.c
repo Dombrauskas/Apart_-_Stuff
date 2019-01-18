7e50246c3f775bf9f64c23f00a2d391736e6c9b4
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-01-17 22:51:19 -0200

    Create goldbach.c
    This program shows all prime numbers between 1 and the number chosen, then a mathematic expression of two prime numbers which results in the even number chosen.

---
+++ goldbach.c
@@ -0,0 +1,80 @@
+/*
+ *
+ * autor Maurício Freire
+ * A Conjectura de Goldbach diz que todo número par maior que 2 pode ser obtido pela
+ * soma de dois números primos inferiores a ele.
+ * The Goldbach's Conjecture says that any even number greater than 2 may be written
+ * as the sum of two prime numbers.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void nums_prims(int);
+void Goldbach(int*, int);
+
+int main()
+{
+    int x;
+    printf("Digite um numero: ");
+    scanf("%d", &x);
+    nums_prims(x);
+    return 0;
+}
+
+/*
+ * Preenche um vetor com todos os números primos dentro do limite escolhido.
+ * Fill an array with all the prime numbers within the boundaries chosen.
+*/
+void nums_prims(int a)
+{
+    int i, j, p = 0, v[a/2], c = 0, u;
+    for (i = 1; i <= a; i++) {
+        for (j = 1; j <= i; j++)
+            if (i % j == 0) p++;
+        
+        if (p <= 2) v[c++] = i;
+        p = 0;
+    }
+    
+    printf("Numeros primos entre 1 e %d:\n", a);
+    for (i = 0; v[i] != '\0'; i++)
+        printf("Numero primo #%02d: %d\n", i+1, v[i]);
+        
+    printf("Digite um numero par: ");
+    scanf("%d", &u);
+    //Se o número for ímpar ele será decrementado antes de ser passado para a função.
+    //If the number is odd it shall be decremented before being passed to the function.
+    if (u % 2 == 0) Goldbach(v, u);
+    else Goldbach(v, --u);
+}
+
+/*
+ * Função para encontrar uma soma possível (pode haver mais de uma) de
+ * primos para formar a expressão.
+ * Function to encounter a possible sum (there might have plus than one)
+ * of primes to make the expression.
+*/
+void Goldbach(int *s, int g)
+{
+    printf("\n");
+    int i, j, k;
+    bool flag = false;
+    for (i = 0; i < g; i ++)
+        if (s[i] > g) break;
+    
+    //Volta uma posição no vetor para usar o maior primo abaixo de g.
+    //Moves the array one position back to use the greater prime smaller than g.
+    --i;
+    for (j = i; j >= 0; j--) {
+        for (k = 0; k < i; k++) {
+            if (s[j] + s[k] == g) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag) break;
+    }
+    printf("%d + %d = %d\n", s[k], s[j], g);
+}
