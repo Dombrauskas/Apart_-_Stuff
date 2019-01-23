f7b7deab734fe3fe6ec1c4b8a6093d049076c560
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-01-23 15:45:21 -0200

    Update simples.py

--- simples.py
+++ simples.py
@@ -1,6 +1,6 @@
 print "As Grandes Voltas!"
 tf = "Tour de France"
-lv = "La Volta d'España"
+lv = "La Volta d'Espana"
 gi = "Giro d'Italia"
 
-print tf"\n"lv"\n"gi"\n"
+print tf, "\n", lv, "\n", gi, "\n"
