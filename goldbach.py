6c76cb51cfbf38e5f291a5e732790fa4ec7f510c
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-01-28 20:14:14 -0200

    Update goldbach.py

--- goldbach.py
+++ goldbach.py
@@ -1,6 +1,16 @@
-# Idem ao anteriores.
-# Equal to the others.
-    
+'''
+ '
+ ' autor Maurício Freire
+ ' A Conjectura de Goldbach diz que todo número par maior que 2 pode ser obtido pela
+ ' soma de dois números primos inferiores a ele.
+ ' The Goldbach's Conjecture says that any even number greater than 2 may be written
+ ' as the sum of two prime numbers.
+'''
+
+'''
+ ' Preenche um vetor com todos os números primos dentro do limite escolhido.
+ ' Fill an array with all the prime numbers within the chosen boundaries.
+'''
 def primos(p):
     x = 0
     c = x
@@ -16,20 +26,42 @@ def primos(p):
     print(v)
     Goldbach(v, p)
 
+'''
+ ' Função para encontrar uma soma possível (pode haver mais de uma) de
+ ' primos para formar a expressão.
+ ' Function to encounter a possible sum (there might have more than one)
+ ' of primes to make the expression.
+'''
 def Goldbach(v, p):
-    flag = False
     g = (int(input("\nDigite um número inferior a ")))
     print(p, ">>>", g)
     
+    # Evita escolha de números superiores ao maior primo.
+    # Avoid choosing numbers greater than the largest prime.
+    if g > p:
+        while g > p:
+            g = (int(input("Digite um número inferior a ")))
+            print(p, ">>>", g)
+    
+    # Decrementa um se o número for ímpar.
+    # If the number is odd it is decremented by one.
+    if g % 2 != 0:
+        g -= 1
+    
+    # Elege o maior primo abaixo do número escolhido.
+    # Elect the greater prime under the number chosen.
     for i in range(g):
         if v[i] > g:
+            i -= 1
             break
-        
+    
+    # Exibe as somas possíveis.
+    # Shows the possible sums.
     for j in range(0,i):
         for k in range(0,i+1):
             if v[j] + v[k] == g:
                 print(v[j], "+", v[k], "=", g)
-    
+                
 p = (int(input("Digite um número: ")))
 print(p,"\n")
 primos(p)
