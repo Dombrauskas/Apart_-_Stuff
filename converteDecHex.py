950fb83af2dd7ae681b90afc06f8451722cf4d39
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-02-09 19:53:10 -0200

    Update converteDecHex.py

--- converteDecHex.py
+++ converteDecHex.py
@@ -4,34 +4,33 @@
 #
 #
 
+def hex(m):
+    print(m)
+    if m > 15:
+        while m > 15:
+            m //= 16
+            if m > 15:
+                hex(m)
+            elif m > 9:
+                if m == 10:
+                    bin.append('A')
+                if m == 11:
+                    bin.append('B')
+                if m == 12:
+                    bin.append('C')
+                if m == 13:
+                    bin.append('D')
+                if m == 14:
+                    bin.append('E')
+                if m == 15:
+                    bin.append('F')
+            else:
+                bin.append(m)
+
+
 m = (int(input()))#"Digite um número: ")))
 bin = []
 
-'''
-while m > 0:
-    if m > 16:
-        m //= 16
-    elif m % 16 >= 10:
-        if m == 10:
-            bin.append('A')
-        if m == 11:
-            bin.append('B')
-        if m == 12:
-            bin.append('C')
-        if m == 13:
-            bin.append('D')
-        if m == 14:
-            bin.append('E')
-        if m == 15:
-            bin.append('F')
-    else:
-        bin.append(m)
-
+hex(m)
 for i in bin:
     print(i, end="")
-'''
-
-while m > 0:
-    if m > 16:
-        m //= 16
-    print(m)
