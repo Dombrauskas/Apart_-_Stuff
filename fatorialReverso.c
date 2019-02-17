a5c47e0a8a0b7bffb196dd1e9366f3369702390b
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-02-17 20:59:06 -0300

    Update fatorialReverso.c

--- fatorialReverso.c
+++ fatorialReverso.c
@@ -2,9 +2,8 @@
  * 
  * autor Mauricio Freire
  * Idem a versão em Python.
- * Idem Python version.
+ * Idem to Python version.
 */
-
 #include <stdio.h>
 
 int rev_fat(int);
@@ -12,21 +11,33 @@ int rev_fat(int);
 int main()
 {
     int f, r;
-    printf("Digite um numero: ");
-    scanf("%d", &f);
     
-    r = rev_fat(f);
-    if (!r)
-        printf("%d nao eh produto fatorial de nenhum numero!\n", f);
-    else
-        printf("%d eh produto fatorial de %d\n", f, r);
+    do {
+        printf("Digite um numero: ");
+        scanf("%d", &f);
+        
+        r = rev_fat(f);
+        if (r == 0)
+            printf("%d não é fatorial de nenhum número!\n", f);
+        else
+            printf("%d é fatorial de %d\n", f, r);
+    } while (f > 1);
     return 0;
 }
 
 int rev_fat(int fat)
 {
-    int v = fat, i;
-    for (i = 1; i < v; i++) {
+    int v, r;
+    v = fat;
+    // Se o valor for ímpar não é produto fatorial de nenhum número.
+    // If the value is odd it is not a factorial product of any number.
+    if (fat % 2 != 0) return 0;
+    
+    for (int i = 1; i <= v; i++) {
+        // Se o resto da divisão não for zero o número não fatorial.
+        // If the module of the division is not zero it is not factorial.
+        r = fat % i;
+        if (r != 0) return 0;
         fat /= i;
         if (fat == 1) return i;
     }
