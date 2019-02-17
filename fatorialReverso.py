a2449fd09ffa58e6e18f49086dd7002ce0faf8e8
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-02-17 18:57:09 -0300

    Update fatorialReverso.py

--- fatorialReverso.py
+++ fatorialReverso.py
@@ -1,15 +1,20 @@
-# Não acabado ainda!!!
+# Dado um número o programa verifica se este é fatorial 
+# de algum número.
+# Given a number the program verifies whether it is the
+# factorial product of any number.
 
-def Fatorial(fat):
-    for i in range(2,fat):
-        fat /= i
-        if (fat == i):
-            return f
-        else:
-            Fatorial(int(fat))
-
-n = (int(input("Digite um número: ")))
-
-f = Fatorial(n)
-
-print(f)
+def rev_fat(fat):
+    v = fat
+    for i in range(1,v):
+        while fat > i:
+            print("{}\t/\t{}\t=\t{}".format(fat, i, int(fat / i)))
+            fat = int(fat / i)
+            if fat == 1:
+                return i
+        
+f = int(input())
+r = rev_fat(f)
+if r == None:
+    print(f,"não é fatorial de nenhum número!")
+else:
+    print("{} é fatorial de {}".format(f,r))
