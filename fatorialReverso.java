9db69189abd9bb83556fbaaf9179899f0f7c1657
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-02-17 20:50:03 -0300

    Update fatorialReverso.java

--- fatorialReverso.java
+++ fatorialReverso.java
@@ -1,18 +1,28 @@
-# Idem a versão em Python.
-# Idem to Python version.
+/**
+ *
+ * @author Mauricio
+ * Idem a versão em Python.
+ * Idem to Python version.
+*/
+import java.util.Scanner;
 
 class rev_Fat {
-    private int v;
+    private int v, r;
     
     int reverseFatorial(int fat) {
         v = fat;
+        // Se o valor for ímpar não é produto fatorial de nenhum número.
+        // If the value is odd it is not a factorial product of any number.
+        if (fat % 2 != 0) return 0;
         
-        for (int i = 1; i < v; i++)
-            while (fat > i) {
-                System.out.println(fat + "" + i);
-                fat /= i;
-                if (fat == 1) return i;
-            }
+        for (int i = 1; i <= v; i++) {
+            // Se o resto da divisão não for zero o número não fatorial.
+            // If the module of the division is not zero it is not factorial.
+            r = fat % i;
+            if (r != 0) return 0;
+            fat /= i;
+            if (fat == 1) return i;
+        }
         return 0;
     }
 }
@@ -20,13 +30,17 @@ class rev_Fat {
 public class Main {
     public static void main(String args[]) {
         rev_Fat ob = new rev_Fat();
-        int f = 120, r;
+        Scanner a = new Scanner(System.in);
+        int f, r;
         
-        r = ob.reverseFatorial(f);
-        
-        if (r == 0)
-            System.out.println(f + " não é fatorial de nenhum número!");
-        else
-            System.out.println(f + " é fatorial de " + r);
+        do {
+            f = a.nextInt();
+            r = ob.reverseFatorial(f);
+            
+            if (r == 0)
+                System.out.println(f + " não é fatorial de nenhum número!");
+            else
+                System.out.println(f + " é fatorial de " + r);
+        } while (f > 1);
     }
 }
