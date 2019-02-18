79bf46e6da3ca3d5eb4175db25efb85a10e5536b
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-02-17 21:13:47 -0300

    Create fatorial.c

---
+++ fatorial.c
@@ -0,0 +1,24 @@
+/**
+ * 
+ * autor Maurício Freire
+ * Calcula o fatorial de um número.
+ * Calculates the fatorial of a number.
+*/
+#include <stdio.h>
+
+int fatorial(int);
+
+int main()
+{
+    int f;
+    printf("Digite um numero: ");
+    scanf("%d", &f);
+    printf("%d\n", f = fatorial(f));
+    return 0;
+}
+
+int fatorial(int f)
+{
+    if (f == 1) return 1;
+    return f * fatorial(f - 1);
+}
