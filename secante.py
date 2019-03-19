065866d815083de35f07a6409cd2a1381009dbd8
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-03-19 18:43:22 -0300

    Add files via upload

---
+++ secante.py
@@ -0,0 +1,29 @@
+import math
+from math import cos as C
+from math import log as ln
+
+def Xn(a, b, ffa, fb):
+    xn = ((a * fb - b * fa) / (fb - fa))
+    if xn > 0:
+        raiz(xn, b)
+    else:
+        raiz(a, xn)
+
+# Calcula os valores de f(a) e f(b).
+# Calculate the values of f(a) e f(b).
+def raiz(x, y):
+    fx = C(x * 3) - ln(x - 3)
+    fy = C(y * 3) - ln(y - 3)
+    print("{} {}".format(fx, fy))
+
+    try:
+        assert fx < 0 and fx > 0
+        Xn(x, y, fx, fy)
+    except:
+        Xn(y, x, fy, fx)
+        #print("Ocorreu um erro durante a execução!")
+
+a = int(input("Intervalo: "))
+b = int(input("Intervalo: "))
+
+raiz(a, b)
