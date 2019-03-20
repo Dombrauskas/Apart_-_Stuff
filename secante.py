19bf65eaee4f644a5045b98de0cb5c89780d33f5
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-03-20 15:54:52 -0300

    Update secante.py
    Funciona, porém preciso fazer mais teste e algumas especificações extras.

--- secante.py
+++ secante.py
@@ -1,7 +1,7 @@
 """
  "
  " Autor Maurício Freire
- " Métoda da Posição Falsa (Secante)
+ " Método da Posição Falsa (Secante)
  " False Position Method (Secant)
 """
 
@@ -9,28 +9,43 @@ import math
 from math import cos as C
 from math import log as ln
 
-def Xn(a, b, fa, fb):
+def Xn(a, b, fa, fb, n, p):
     xn = ((a * fb - b * fa) / (fb - fa))
-    if xn > 0:
-        raiz(xn, b)
-    else:
-        raiz(a, xn)
+    fxn = C(xn * 3) - ln(xn - 3)
 
+    # Chamar o critério de parada antes de chamar a raiz outra vez.
+    # Ainda pensando em como definir a substituição de a ou b por xn.
+    erro(fxn, xn)
+    
+    if xn < 0:
+        raiz(xn, p)
+    else:
+        raiz(n, xn)
+
+# Estabelece o critério de parada.
+# Establishes the ending criterion.
+def erro(fxn, xn):
+    E = 10**-2
+    m = (fxn + E) * (fxn - E)
+    if (m < E):
+        print("\nRaiz encontrada: {}".format(xn))
+        raise Exception
+   
 # Calcula os valores de f(a) e f(b).
 # Calculate the values of f(a) e f(b).
 def raiz(x, y):
     fx = C(x * 3) - ln(x - 3)
     fy = C(y * 3) - ln(y - 3)
-    print("{} {}".format(fx, fy))
-
-    try:
-        assert fx < 0 and fx > 0
-        Xn(x, y, fx, fy)
-    except:
-        Xn(y, x, fy, fx)
-        #print("Ocorreu um erro durante a execução!")
+        
+    if fx < 0 and fy > 0:
+        Xn(x, y, fx, fy, x, y)
+    else:
+        Xn(x, y, fx, fy, y, x)
 
 a = int(input("Intervalo: "))
 b = int(input("Intervalo: "))
 
-raiz(a, b)
+try:
+    raiz(a, b)
+except:
+    print()
