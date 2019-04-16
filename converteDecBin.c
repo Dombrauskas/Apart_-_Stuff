6e7fd2607922fa1dca868fc2996764afdbc8c155
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-04-15 23:46:14 -0300

    Update converteDecBin.c

--- converteDecBin.c
+++ converteDecBin.c
@@ -8,18 +8,19 @@
 
 int main()
 {
-    int i, n = 170;
-    int bin[7];
+    int i, n;
+    int bin[16];
 
+    printf("Informe um numero (decimal): ");
+    scanf("%d", &n);
+    
     printf("%d em binario:\n", n);
     for (i = 15; i >= 0; i--) {
-        if (n % 2 == 0) {
+        if (n % 2 == 0)
             bin[i] = 0;
-            n = n / 2;
-        } else {
+        else
             bin[i] = 1;
-            n = n / 2;
-        }
+        n /= 2;
     }
 
     for (i = 0; i < 16; i++) {
