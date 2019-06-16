1cccc5cf2a3cf759e06c656b1a6f19dc578dd94b
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2019-06-16 20:42:13 -0300

    Update escolha.py

--- escolha.py
+++ escolha.py
@@ -5,6 +5,10 @@ import os
 def preludio():
     print("Prelúdio: ")
 
+def continua():
+    x = input("\n\nPrecione ENTER para continuar...")
+    print(x)
+
 
 def celeiro():
     preludio()
@@ -53,15 +57,18 @@ def celeiro():
                             break
                     # Quebrar a porta do Celeiro
                     if x.capitalize() == 'P':
-                        print("O trinco se rompe e a porta abre o homem acelera, mas você foge.")
+                        print("\nO trinco se rompe e a porta abre o homem acelera, mas você foge.")
+                        continua()
                         return "Fugitivo"
                     # Lutando com a pá
                     else:
-                        print("Na Pá")
-                        return "Teste"
+                        print("\nVocê pega a pá enquanto o Homem com o Machado se aproxima. Ele ataca, mas você desvia e contra-golpeia.")
+                        print("Ele sangra e se zanga; em fúria extrema ele afunda o machado em seu peito\ncom tanta força que suas costelas saem pelas costas.")
+                        continua()
+                        return "Fatality"
                 # Escolha da porta
                 else:
-                    print("Próximo a você a um monte de feno que você pode se esconder. Há também uma janela no alto,\ntalvez você possa alcançá-la e fugir por lá.\nFeno ou Janela? F / J")
+                    print("\nPróximo a você a um monte de feno que você pode se esconder. Há também uma janela no alto,\ntalvez você possa alcançá-la e fugir por lá.\nFeno ou Janela? F / J")
 
                     while True:
                         x = input()
@@ -69,12 +76,14 @@ def celeiro():
                             break
                     # No palheiro
                     if x.capitalize() == 'F':
-                        print("Você se joga no monte de feno e espeta a mão. Você achou uma agulha no palheiro.")
+                        print("\nVocê se joga no monte de feno e espeta a mão. Você achou uma agulha no palheiro.")
+                        continua()
                         return "Vencedor"
                     # Na janela
                     else:
-                        print("Você tenta e não alcança, mas ainda tem esperança. Tenta mais vez e consegue pela ponta dos dedos.\nCom esforço se levanta para a passagem, porém você não cabe nela, você se debate e se esforça;")
+                        print("\nVocê tenta e não alcança, mas ainda tem esperança. Tenta mais vez e consegue pela ponta dos dedos.\nCom esforço se levanta para a passagem, porém você não cabe nela, você se debate e se esforça;")
                         print("Logo você sente o frio metal do machado passando por você, o sabor do sangue lhe vem a boca e\nvocê tem certeza...")
+                        continua()
                         return "Morri"
             # Esconde
             else:
@@ -98,14 +107,18 @@ def celeiro():
                             if x.capitalize() == 'S':
                                 print("No desespero você salta para fora o que surpreende o Homem com o Machado e vai rumo a oficina.\nNão há saída lá, então você pega uma enxada.")
                                 print("O machado vai em sua direção e você interpõe a enxada no reflexo de defesa.\nA enxada se quebra e o Homem com o Machado te acerta com o cabo e você desmaia.")
+                                continua()
                                 return "Desmaio"
                             # Fique na caçamba
                             else:
                                 print("A próxima machadada é certeira")
+                                continua()
                                 return "Alarmado"
         # No primeiro andar
         else:
-            print("\nVocê precisa se proteger o homem com machado está vindo te matar. Você pode tirar um cavalo do celeiro e fugir mesmo sem saber\ncomo cavalgar ou pode se esconder por entre o monte de palha. Fugir ou Esconder? F / e - Sem")
+            print("\nVocê precisa se proteger o homem com machado está vindo te matar. Você pode tirar " +
+            "um cavalo do celeiro e fugir mesmo sem saber como cavalgar ou pode se esconder por entre o " +
+            "monte de palha. Fugir ou Esconder? F / e - Sem")
             
             while True:
                 x = input()
@@ -121,7 +134,7 @@ def celeiro():
                         break
                 # Para quê serve a cela?!
                 if x.capitalize() == 'S':
-                    print("\nO cavalo se irrita, relincha e empina, você se afasta e esbarra no Homem com o Machado,\nele move o machado para te atacar. Você abaixa ou pula para trás? B / P")
+                    print("\nO cavalo se irrita, relincha e empina, você se afasta e esbarra no Homem com o Machado,\nele move o machado para te atacar. Você abaixa ou pula para trás? B / p - Sem")
 
                     while True:
                         x = input()
@@ -138,6 +151,7 @@ def celeiro():
                         # Vou Lutar
                         if x.capitalize() == 'A':
                             print("Ao avançar em sua direção o Homem olha para você e mesmo por baixo da máscara os olhos dele fixos em você te deixam congelado.")
+                            continua()
                             return "Medo"
                 # Caindo do cavalo
                 else:
@@ -149,6 +163,7 @@ def celeiro():
                         x = input()
                         if x.capitalize() == 'S':
                             print("Ops! Sua perna foi quebrada na queda. O Homem com o Machado se aproxima e corta sua cabeça.")
+                            continua()
                             return "Decaptação"
                         elif x.capitalize() == 'N':
                             print("Sem escolha você decide confrontá-lo; o machado dele ficou preso ao cavalo, você pega o machado ou vai no X-1? M / X")
@@ -158,41 +173,61 @@ def celeiro():
                     # O machado é meu
                     if x.capitalize() == 'M':      
                         print("O cavalo ainda vivo e com dor reage por reflexo quando você puxa o machado\nele te dá um coice que te derruba.")
+                        continua()
                         return "Inconsciênte"
                     elif x.capitalize() == 'X':
                         print("Você se põe em posição o seu oponente não se move, apenas te observa. Dá um passo para trás e some dentro do celeiro.")
+                        continua()
                         return "Sobrevivente"
             
     # Fora do Celeiro
     else:
         x = "WWW"
-        print("\nVocê dá a volta no celeiro e vê três pessoas encapuzadas esfaqueando alguém que berra incontrolavelmente.\nVocê salva a pessoa ou a ignora?\nS / I")
-        while x.capitalize() != 'S' or x.capitalize() != 'I':
+        print("\nVocê dá a volta no celeiro e vê três pessoas encapuzadas esfaqueando alguém que berra " +
+        "incontrolavelmente.\nVocê salva a pessoa ou a ignora?\nS / i - Sem")
+
+        while True:
             x = input()
+            if x.capitalize() == 'S':
+                break
+        # Bom Samaritano
         if x.capitalize() == 'S':
-            print("\nVocê corre para cima deles saltando nas costas do indivíduo mais próximo,\nderrubando sua faca e fazendo os outros dois pararem de esquear a vítima.\nVocê pega a faca dele e...")
-            print("Continua atacando o mesmo indivíduo, defende-se daquele tenta te atacar ou desvia dele e parte para cima do terceiro?\nIndivíduo: 1, 2 ou 3")
-            i = 10
+            print("\nVocê corre para cima deles saltando nas costas do indivíduo mais próximo, derrubando " + 
+            "sua faca e fazendo os outros dois pararem de esquear a vítima.\nVocê pega a faca dele e...")
+            print("Continua atacando o mesmo indivíduo, defende-se daquele tenta te atacar ou desvia dele e " + 
+            "parte para cima do terceiro?\nIndivíduo: 1, 2 ou 3")
             
-            while i > 3:
+            i = 0
+            while i > 3 or i < 1:
                 i = int(input())
+            # Terminando o serviço
             if i == 1:
-                print("\nVocê corta a garganta dele e é pego pelo braço enquanto o outro te acerta na barriga.\nVocê dá uma cabeçada em quem te segura e consegue se livrar e se ajoelha no chão com dor.")
-                print("Olhando para o lado você nota que a vítima é na realidade um porco e é chutado na cara,\ncaído no chão o outro levanta a faca para fincá-la em você. Rolar para esquerda ou direta? E / D")
+                print("\nVocê corta a garganta dele e é pego pelo braço enquanto o outro te acerta na barriga. " +
+                "Você dá uma cabeçada em quem te segura e consegue se livrar e se ajoelha no chão com dor. " +
+                "Olhando para o lado você nota que a vítima é na realidade um porco e é chutado na cara, caído " + 
+                "no chão o outro levanta a faca para fincá-la em você. Rolar para esquerda ou direta? E / d - Sem")
                 
-                while x.capitalize() != 'E' or x.capitalize() != 'D':
+                while True:
                     x = input()
-                if x == 'E':
-                    print("Você esbarra no cadáver que você matou e é atingido no peito pela faca e morre")
+                    if x.capitalize() == 'E':
+                        break
+                # Dois cadáveres
+                if x.capitalize() == 'E':
+                    print("\nVocê esbarra no cadáver que você matou e é atingido no peito pela faca e morre")
+                    continua()
                     return "Esfaqueado"
 
                 
 l = list()
-n = int(input("Quantas pessoas vão jogar? "))
 r = {}
+n = 0
+while n <= 0 or n > 10:
+    n = int(input("Quantas pessoas vão jogar? "))
 
 for j in range(n):
     l.append(input())
+
+os.system('clear') or None
 
 for e in range(n):
     r[l[e]] = celeiro().upper()
@@ -203,5 +238,4 @@ for e in range(n):
 
 for k in r:
   print(k, r[k])
-
 
