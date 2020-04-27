78fd396ec9724640c02b746c89eef34b330f62b3
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-04-27 17:27:49 -0300

    Add files via upload

--- Collatz/collatz.py
+++ Collatz/collatz.py
@@ -2,7 +2,7 @@
  " 
  " Autor Maurício Freire
  " A Conjectura de Collatz ou o Problema de Syracuse é um problema matemático sem
- " solução, pois os cálculos nunca cessão. O problema consiste em ao escolher um
+ " solução, pois os cálculos nunca cessam. O problema consiste em ao escolher um
  " número natural e, sendo ele par divida-o por 2 ou sendo ímpar, multiplique-o
  " por 3 e some 1; os resultados sempre chegarão ao número 1, entretanto os três
  " últimos passos sempre se repetirão. 1 * 3 + 1 = 4 ; 4 / 2 = 2 ; 2 / 2 = 1 ; ...
@@ -10,7 +10,7 @@
  " because the calcules never cease. The problem consists in choosing a natural
  " number and then, divides it by 2 if it is even or multiplies by 3 and sum 1 up
  " if it is odd; it will always result in 1, however the three last steps will
- " infitely repeat. 1 * 3 + 1 = 4 ; 4 / 2 = 2 ; 2 / 2 = 1 ; ...
+ " endlessly repeat. 1 * 3 + 1 = 4 ; 4 / 2 = 2 ; 2 / 2 = 1 ; ...
 """
 
 def Collatz(x):
@@ -27,6 +27,8 @@ def Collatz(x):
         # Since it reaches one at first the program will stop running.
         Collatz(x)
 
+
 num = int(input("Digite um Número Natural: "))
 
 Collatz(num)
+
