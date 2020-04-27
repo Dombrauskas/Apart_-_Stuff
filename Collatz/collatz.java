0b51aae82163eec446724d2d9371991d3c7e939f
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-04-27 17:25:38 -0300

    Add files via upload

--- Collatz/collatz.java
+++ Collatz/collatz.java
@@ -1,8 +1,8 @@
 /**
  *
- * Autor Maurício Freire
+ * @author Maurício Freire
  * A Conjectura de Collatz ou o Problema de Syracuse é um problema matemático sem
- * solução, pois os cálculos nunca cessão. O problema consiste em ao escolher um
+ * solução, pois os cálculos nunca cessam. O problema consiste em ao escolher um
  * número natural e, sendo ele par divida-o por 2 ou sendo ímpar, multiplique-o
  * por 3 e some 1; os resultados sempre chegarão ao número 1, entretanto os três
  * últimos passos sempre se repetirão. 1 * 3 + 1 = 4 ; 4 / 2 = 2 ; 2 / 2 = 1 ; ...
@@ -10,8 +10,9 @@
  * because the calcules never cease. The problem consists in choosing a natural
  * number and then, divides it by 2 if it is even or multiplies by 3 and sum 1 up
  * if it is odd; it will always result in 1, however the three last steps will
- * infitely repeat. 1 * 3 + 1 = 4 ; 4 / 2 = 2 ; 2 / 2 = 1 ; ...
+ * endlessly repeat. 1 * 3 + 1 = 4 ; 4 / 2 = 2 ; 2 / 2 = 1 ; ...
 */
+
 import java.util.Scanner;
 
 class Conjectura {
@@ -40,9 +41,10 @@ public class Main {
         
         System.out.print("Digite um Número Natural: ");
         num = N.nextInt();
-
+        
         System.out.println(num);
-
+        
         ob.Collatz(num);    
     }
 }
+
