4bcf833589660d36d194ae4d81a6192188d6d913
Dombrauskas <43318381+Dombrauskas@users.noreply.github.com>
2020-04-27 17:31:55 -0300

    Add files via upload

--- Collatz/collatz.rb
+++ Collatz/collatz.rb
@@ -10,7 +10,7 @@
   because the calcules never cease. The problem consists in choosing a natural
   number and then, divides it by 2 if it is even or multiplies by 3 and sum 1 up
   if it is odd; it will always result in 1, however the three last steps will
-  infitely repeat. 1 * 3 + 1 = 4 ; 4 / 2 = 2 ; 2 / 2 = 1 ; ...
+  endlessly repeat. 1 * 3 + 1 = 4 ; 4 / 2 = 2 ; 2 / 2 = 1 ; ...
 =end
 
 def Collatz(x)
@@ -31,3 +31,4 @@ end
 print "Informe um número natural: "
 n = gets.chomp.to_i
 Collatz(n)
+
